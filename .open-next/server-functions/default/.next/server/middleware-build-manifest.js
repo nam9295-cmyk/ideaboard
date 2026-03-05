b0887905617196580c6f08f3186c7b1a203822c6
남형우 <nam9295@namhyeong-uui-MacBookAir.local>
2026-03-05 15:41:02 +0900

    Deploy via Cloudflare with token

---
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/4b9eae0c8dc7e975.js",
+    "static/chunks/4636862c8348783f.js",
+    "static/chunks/a90b6d11afe742e2.js",
+    "static/chunks/turbopack-ad639bfae1db2503.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pOjvoNayAHQmTNKvQZzLnNmxYT1Hag2+Cy8K9OGb6vA=\"\n}"
